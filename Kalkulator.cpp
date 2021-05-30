50dcba0f1f120d49cdea2ad07e93c1adf4d5e576
Ana Puljic <apuljic05@gmail.com>
2021-05-30 19:28:07 +0200

    Potenciranje

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <math.h>
+#include<cmath>
 
 int main (void){
 
-	int a, b, c, select;
+	double a, b, c;
+	int select;
 	
 	printf("Unesi prvi broj: ");
 	scanf("%d", &a);
 	printf("Unesi drugi broj: ");
 	scanf("%d",  &b);
 	
-	while(select != 1 && select != 2 && select != 3 && select != 4){
-		printf("\n1 = Zbrajanje\n2 = Oduzimanje\n3 = Mnozenje\n4 = Dijeljenje\nOdaberi funkciju:");
+	while(select != 1 && select != 2 && select != 3 && select != 4 && select != 5){
+		printf("\n1 = Zbrajanje\n2 = Oduzimanje\n3 = Mnozenje\n4 = Dijeljenje\n5 = Potenciranje\nOdaberi funkciju:");
 		scanf("%d", &select);
 	}
 	
@@ -31,10 +34,13 @@ int main (void){
 			break;
 			
 		case 4:
-			float c;
 			c = a / b;
 			printf("\nDijeljenje: %d / %d = %.2f", a, b, c);
 			break;
+			
+		case 5:
+			c = pow (a, b);
+			printf("\nPotenciranje: %d ^ %d = %lf", a, b, c);
 	}
 	
 	return 0;
