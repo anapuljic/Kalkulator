4d6da07958589669492e4c46fe10d056af999f4d
Ana Puljic <apuljic05@gmail.com>
2021-05-30 18:41:12 +0200

    Loop

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -2,15 +2,40 @@
 
 int main (void){
 
-	int a, b, c;
+	int a, b, c, select;
 	
 	printf("Unesi prvi broj: ");
-	scanf("%d", a);
+	scanf("%d", &a);
 	printf("Unesi drugi broj: ");
-	scanf("%d",  b);
+	scanf("%d",  &b);
 	
-	c = a + b;
+	while(select != 1 && select != 2 && select != 3 && select != 4){
+		printf("\n1 = Zbrajanje\n2 = Oduzimanje\n3 = Mnozenje\n4 = Dijeljenje\nOdaberi funkciju:");
+		scanf("%d", &select);
+	}
 	
-	printf("%d + %d = %d", a, b, c);
-
+	switch (select){
+		case 1:
+			c = a + b;
+			printf("\nZbrajanje: %d + %d = %d", a, b, c);
+			break;
+			
+		case 2:
+			c = a - b;
+			printf("\nOduzimanje: %d - %d = %d", a, b, c);
+			break;
+			
+		case 3:
+			c = a * b;
+			printf("\nMnozenje: %d * %d = %d", a, b, c);
+			break;
+			
+		case 4:
+			float c;
+			c = a / b;
+			printf("\nDijeljenje: %d / %d = %.2f", a, b, c);
+			break;
+	}
+	
+	return 0;
 }
