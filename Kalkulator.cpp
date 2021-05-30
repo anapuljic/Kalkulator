ae89bee6ba724755e7d10137bcfdd47eda99c25d
anapuljic <apuljic05@gmail.com>
2021-05-30 18:02:58 +0200

    Prvi commit

---
+++ Kalkulator.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void){
+
+	int a, b, c;
+	
+	printf("Unesi prvi broj: ");
+	scanf("%d", a);
+	printf("Unesi drugi broj: ");
+	scanf("%d",  b);
+	
+	c = a + b;
+	
+	printf("%d + %d = %d", a, b, c);
+
+}
